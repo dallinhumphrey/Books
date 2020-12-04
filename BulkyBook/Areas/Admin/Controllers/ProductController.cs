af0bac80bddab28e447be8fa7b900469a917b076
dallinhumphrey <dallinhumphrey@gmail.com>
2020-12-04 11:35:55 -0700

    added product view

--- BulkyBook/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -84,7 +84,7 @@ namespace BulkyBook.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            var allObj = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType,");
             return Json(new { data = allObj });
         }
 
